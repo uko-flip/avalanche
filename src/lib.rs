152c830285f725b274b4d6d3524a8a55e57565ac
avalanche-lang <avalanche.lang@gmail.com>
2021-02-22 15:21:12 +0300

    Remove optional dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,4 @@ pub mod lexer;
 
 #[cfg(test)]
 mod tests {
-    use rustc_version::version;
-
-    #[test]
-    fn test_compiler_version() {
-        assert!(version().unwrap().major >= 1);
-    }
 }
